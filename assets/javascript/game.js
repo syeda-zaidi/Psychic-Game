79729bf68f3db6654dbc8c3f4c95fa7abd5a4759
syeda zaidi <zaidisyeda03@gmail.com>
2019-06-27 16:18:00 -0700

    changes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,15 +9,10 @@ var guesses = 3;
 var compSelection;
 var guessedLetter = [];
 
-var winsText = document.getElementById ("wins-text");
-var winsLosses = document.getElementById ("losses-text");
-var guessesText = document.getElementById ("guesses-text");
-var userGuesses = document.getElementById ("user-guesses");
-
-winsText.textContent = ("wins : " + wins);
-winsLosses.textContent = ("losses : " + losses);
-guessesText.textContent = ("Guesses Left : " + guesses);
-userGuesses.textContent = ("Guessed Letters so far : " + guessedLetter);
+var winsText = document.getElementById("wins-text");
+var lossesText = document.getElementById("losses-text");
+var guessesText = document.getElementById("guesses-text");
+var userGuesses = document.getElementById("user-guesses");
 
 // setting compchoice var to make a random choice from the var letters 
 
@@ -32,11 +27,20 @@ compSelection = compChoice();
 function reset() {
     guesses = 3;
     compSelection = compChoice();
+    guessedLetter = [];
 }
+
+
+winsText.textContent = ("wins : " + wins);
+lossesText.textContent = ("losses : " + losses);
+guessesText.textContent = ("Guesses Left : " + guesses);
+userGuesses.textContent = ("Guessed Letters so far : " + guessedLetter);
 
 document.onkeyup = function (event) {
     var userGuess = event.key;
     console.log("user chose " + userGuess);
+    guessedLetter.push(userGuess);
+
 
     if (userGuess === compSelection) {
         alert("you guessed right !!");
@@ -45,23 +49,26 @@ document.onkeyup = function (event) {
         reset();
     }
 
-    else if (userGuess !== compSelection && guesses !== 0) {
-        alert("you guessed wrong... try again");
+    else if (userGuess !== compSelection && guesses > 0) {
         guesses--;
-
-        alert("No of guesses left " + guesses);
 
         if (guesses === 0) {
             losses++;
-            alert("you lost " + losses + "time");
 
             reset();
         }
     }
-    winsText.textContent=("wins : " + wins);
-    
+
+
+    winsText.textContent = ("wins : " + wins);
+    lossesText.textContent = ("losses : " + losses);
+    guessesText.textContent = ("Guesses Left : " + guesses);
+    userGuesses.textContent = ("Guessed Letters so far : " + guessedLetter);
 
 }
+
+
+
 
 
 // else if (userGuess !== letters) {
